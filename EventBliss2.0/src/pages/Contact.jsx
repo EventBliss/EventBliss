b8eb1ca873e055146a43e859ae98b4002c6ced84
paola1611 <dp673162@gmail.com>
2024-03-21 17:35:10 -0400

    agregando el logo

--- EventBliss2.0/src/pages/Contact.jsx
+++ EventBliss2.0/src/pages/Contact.jsx
@@ -104,7 +104,7 @@ export function Contact() {
                         </label>
                         <input
                             className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                            id="grid-first-name" type="text" placeholder="Tetsuo"/>
+                            id="grid-first-name" type="text" placeholder="Lucas"/>
                     </div>
                     <div className="w-full md:w-1/2 px-3">
                         <label className="block uppercase tracking-wide text-[#FD8B11] text-xs font-bold mb-2"
@@ -113,7 +113,7 @@ export function Contact() {
                         </label>
                         <input
                             className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-last-name" type="text" placeholder="Arai"/>
+                            id="grid-last-name" type="text" placeholder="Gomez"/>
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
