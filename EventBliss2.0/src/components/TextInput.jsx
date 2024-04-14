7caefdc821cd0e0c9bd19c0ec0fd604d89200042
Christalpena <christalperez0@gmail.com>
2024-04-13 23:37:20 -0400

    Form to edit the orginizer information

--- EventBliss2.0/src/components/TextInput.jsx
+++ EventBliss2.0/src/components/TextInput.jsx
@@ -1,7 +1,7 @@
 import { TextInput } from '@tremor/react';
 
 // eslint-disable-next-line react/prop-types
-export function TextInputComp({ name, label, placeholder, type, register }) {
+export function TextInputComp({ name, label, placeholder, type, register,disabled }) {
   return (
     <div className="w-full px-3 mb-6">
       <label
@@ -12,6 +12,7 @@ export function TextInputComp({ name, label, placeholder, type, register }) {
       </label>
       <TextInput
         type={type ? type : 'text'}
+        disabled={disabled ? true: false}
         placeholder={placeholder}
         {...register(name, { required: type == 'url' ? false : true })} // Pass register function with the field name and validation rules
       />
