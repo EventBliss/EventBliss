9346c18deffd56266b6a066583fadbd8ec47e4ed
Christalpena <christalperez0@gmail.com>
2024-04-17 19:40:29 -0400

    new changes

--- EventBliss2.0/src/pages/organizer/request/components/Actions.jsx
+++ EventBliss2.0/src/pages/organizer/request/components/Actions.jsx
@@ -1,6 +1,10 @@
 import { Confirm } from "../../../../components/Confirm"
+import { updateEventRequest } from "../../../../components/api/request/put"
 // importas la funcion de la api que quieras llamar 
-export function Actions({id, APIFunction, status}) {
+// eslint-disable-next-line react/prop-types
+
+/////PACKAGE RESQUESTS
+export function Actions({id, status, queryKey}) {
 
     const showNoneOption = status === "Finished" || status === "Denied"
     const showFinishedoption = status === "Approved"
@@ -10,10 +14,10 @@ export function Actions({id, APIFunction, status}) {
         {!showNoneOption && !showFinishedoption &&(
             <div className="flex justify-evenly">
                 <div>
-                    <Confirm name={"Denied"} className={"grid mx-auto py-2 shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold px-4 rounded-lg"}  id={id} APIFunction={APIFunction} queryKey={""}></Confirm>
+                    <Confirm name={"Denied"} className={"grid mx-auto py-2 shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold px-4 rounded-lg"}  id={id} APIFunction={updateEventRequest()} queryKey={queryKey}></Confirm>
                 </div>
                 <div>
-                    <Confirm name={"Approved"} className={"grid mx-auto py-2 shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold px-4 rounded-lg"}  id={id} APIFunction={APIFunction} queryKey={""}></Confirm>
+                    <Confirm name={"Approved"} className={"grid mx-auto py-2 shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold px-4 rounded-lg"}  id={id} APIFunction={""} queryKey={queryKey}></Confirm>
                 </div>   
              </div>
         )}
@@ -24,7 +28,7 @@ export function Actions({id, APIFunction, status}) {
 
         {showFinishedoption &&(
             <div>
-                <Confirm name={"Finished"} className={"grid mx-auto py-2 shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold px-4 rounded-lg"}  id={id} APIFunction={APIFunction} queryKey={""}></Confirm>
+                <Confirm name={"Finished"} className={"grid mx-auto py-2 shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold px-4 rounded-lg"}  id={id} APIFunction={APIFunction} queryKey={queryKey}></Confirm>
             </div>
         )}
         
